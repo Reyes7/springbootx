886cdcafb656918c8d1acdb921be72647b5171ee
Reyes7 <marcin.rejewski@gmail.com>
2015-07-10 12:38:45 +0200

    .

--- src/main/java/rest/Application.java
+++ src/main/java/rest/Application.java
@@ -1,11 +1,14 @@
+package rest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 //@Controller
 @EnableAutoConfiguration
-//@Configuration
-@ComponentScan
+@Configuration
+@ComponentScan(basePackages = "rest")
 public class Application {
 
 //    @RequestMapping("/")
