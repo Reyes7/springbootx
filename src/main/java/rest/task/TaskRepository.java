bd094cf380c9472170ca0c2745d0a4798c7c878c
Reyes7 <marcin.rejewski@gmail.com>
2015-07-10 15:15:37 +0200

    updated task service

--- src/main/java/rest/task/TaskRepository.java
+++ src/main/java/rest/task/TaskRepository.java
@@ -1,8 +1,9 @@
 package rest.task;
 
 import org.springframework.stereotype.Repository;
-
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -12,5 +13,32 @@ public class TaskRepository {
 
     public static Map<Long, Task> getTasks() {
         return tasks;
+    }
+
+    public Task getById(long id) {
+        Task task = tasks.get(id);
+        return task;
+    }
+
+    public List<Task> getAll() {
+        return new ArrayList<Task>(tasks.values());
+    }
+
+    public Task add(Task task) {
+        task.setId(tasks.size() + 1);
+        tasks.put(task.getId(), task);
+        return task;
+    }
+
+    public Task update(Task task) {
+        if (task.getId() <= 0) {
+            return null;
+        }
+        tasks.put(task.getId(), task);
+        return task;
+    }
+
+    public void remove(long id) {
+        tasks.remove(id);
     }
 }
--- src/main/java/rest/task/TaskService.java
+++ src/main/java/rest/task/TaskService.java
@@ -3,9 +3,7 @@ package rest.task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class TaskService {
@@ -13,39 +11,25 @@ public class TaskService {
     @Autowired
     TaskRepository taskRepository;
 
-    private Map<Long, Task> tasks = TaskRepository.getTasks();
-
-    public TaskService(){
-        tasks.put(1L, new Task(1L,"Test task",false));
-    }
-
     public Task getTask(long id) {
-        Task task = tasks.get(id);
-        if(task == null ){
-            return null;
-        }
-        return task;
+        return taskRepository.getById(id);
     }
 
     public List<Task> getAllTasks() {
-        return new ArrayList<Task>(tasks.values());
+        return taskRepository.getAll();
     }
 
     public Task addTask(Task task) {
-        task.setId(tasks.size() + 1);
-        tasks.put(task.getId(), task);
-        return task;
+        return taskRepository.add(task);
     }
 
     public Task updateTask(Task task) {
-        if (task.getId() <= 0) {
-            return null;
-        }
-        tasks.put(task.getId(), task);
-        return task;
+        return taskRepository.update(task);
     }
 
-    public Task removeTask(long id) {
-        return tasks.remove(id);
+    public void removeTask(long id) {
+        taskRepository.remove(id);
     }
+
+    //{"task" : "new task", "done" : true}
 }
