fce5bd828a2ab709e596eb2dc3fc2f2ed3d6e6af
Reyes7 <marcin.rejewski@gmail.com>
2015-07-16 15:50:20 +0200

    Add Tests for UserRepository

--- src/main/java/rest/user/UserService.java
+++ src/main/java/rest/user/UserService.java
@@ -12,8 +12,8 @@ public class UserService {
 
     @PostConstruct
     public void init(){
-        repository.save(new User("John", "Rambo"));
-        repository.save(new User("Johny", "Bravo"));
+        //repository.save(new User("John", "Rambo"));
+        //repository.save(new User("Johny", "Bravo"));
     }
 
     public Iterable<User> getAllUsers() {
