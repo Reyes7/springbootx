66a831efc6e26bfbb5b4e4918bcbea74293d6c01
Reyes7 <marcin.rejewski@gmail.com>
2015-07-20 13:24:49 +0200

    Added autowird Heroku config to Application class

--- src/main/java/rest/Application.java
+++ src/main/java/rest/Application.java
@@ -1,14 +1,20 @@
 package rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import rest.config.HerokuDatabaseConfig;
 
 @ComponentScan(basePackages = "rest")
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
+
+    @Autowired(required = false)
+    HerokuDatabaseConfig herokuDatabaseConfig;
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
