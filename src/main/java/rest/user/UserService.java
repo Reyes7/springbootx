1b55d44c30bd1db40ecb72814eef6484f33eef4b
Reyes7 <marcin.rejewski@gmail.com>
2015-07-21 08:31:08 +0200

    Removed empty init method in UserService class

--- src/main/java/rest/user/UserService.java
+++ src/main/java/rest/user/UserService.java
@@ -1,20 +1,12 @@
 package rest.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository repository;
-
-    @PostConstruct
-    public void init(){
-
-    }
 
     public Iterable<User> getAllUsers() {
         return repository.findAll();
