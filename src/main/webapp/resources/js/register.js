f73a998593bc35fae5eeb5b0d61b9aef718fc568
Reyes7 <marcin.rejewski@gmail.com>
2015-07-23 13:33:40 +0200

    Added user creation with angular

---
+++ src/main/webapp/resources/js/register.js
@@ -0,0 +1,22 @@
+var register_app = angular.module('register', []);
+
+register_app.controller('formCtrl', function($scope, $http) {
+    $scope.user = {firstName:"", lastName:""};
+
+    $scope.submit = function() {
+    	$scope.submitting = true;
+    	$http({
+    	    method: 'POST',
+    	    url: '/users',
+    	    data: $scope.user
+    	}).success(function(data) {
+    	    $scope.submitting = false;
+    	}).error(function(data, status) {
+    	    $scope.submitting = false;
+    	    if (status === 400)
+    		$scope.badRequest = data;
+    	    else if (status === 409)
+    		$scope.badRequest = 'The name is already used.';
+    	});
+    };
+});
