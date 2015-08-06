d2c4841c72cb037d6ff25a18841e96f578d87deb
Reyes7 <marcin.rejewski@gmail.com>
2015-08-06 15:46:36 +0200

    Move flyway configuration from Application class to FlywayConfig class

--- src/main/java/rest/Application.java
+++ src/main/java/rest/Application.java
@@ -1,12 +1,9 @@
 package rest;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import rest.config.HerokuDatabaseConfig;
 
 @ComponentScan(basePackages = "rest")
 @EnableAutoConfiguration
@@ -15,9 +12,5 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
-        Flyway flyway = new Flyway();
-        flyway.setDataSource("jdbc:postgresql://ec2-54-228-180-92.eu-west-1.compute.amazonaws.com:5432/d57a32kv8593as?user=kizocqwgtqcjlc&password=Sn9guNEnwQdDlYKM35B6x9ob76",
-                "kizocqwgtqcjlc","Sn9guNEnwQdDlYKM35B6x9ob76");
-        flyway.migrate();
     }
 }
